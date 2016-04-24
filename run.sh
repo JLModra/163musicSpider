3d29dd39fb8127682aee6d9eb49aa757983d7b87
Modra <754675419@qq.com>
2016-04-25 00:25:59 +0800

    0.0

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+#!/mongodb-linux-x86_64-ubuntu1404-3.2.5/bin
 
-python 163musicSpider.py
+python musicSpider.py
+./mongoexport -d netease_music_list -c music_list_info -o ../../music_list_info.csv
+./mongoexport -d netease_music_list -c debug_info -o ../../debug_info
+python mail.py
