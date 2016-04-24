d50b3ff9fc49ab55a28fd488537fb06306cc3c20
Modra <754675419@qq.com>
2016-04-25 00:36:09 +0800

    dp some change Please enter the commit message for your changes. Lines starting

--- run.sh
+++ run.sh
@@ -2,6 +2,5 @@
 #!/mongodb-linux-x86_64-ubuntu1404-3.2.5/bin
 
 python musicSpider.py
-./mongoexport -d netease_music_list -c music_list_info -o ../../music_list_info.csv
-./mongoexport -d netease_music_list -c debug_info -o ../../debug_info
+./mongodb-linux-x86_64-ubuntu1404-3.2.5/bin/mongoexport -d netease_music_list -c music_list_info -o /163musicSpider/music_list_info.csv
 python mail.py
