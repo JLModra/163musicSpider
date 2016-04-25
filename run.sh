b4cb80217391c102397a120c750ac5a7aff5ed8b
Modra <754675419@qq.com>
2016-04-25 09:53:10 +0800

    solve the route bug

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 #!/mongodb-linux-x86_64-ubuntu1404-3.2.5/bin
 
 python musicSpider.py
-./mongodb-linux-x86_64-ubuntu1404-3.2.5/bin/mongoexport -d netease_music_list -c music_list_info -o /163musicSpider/music_list_info.csv
+../../mongodb-linux-x86_64-ubuntu1404-3.2.5/bin/mongoexport -d netease_music_list -c music_list_info -o /163musicSpider/music_list_info.csv
 python mail.py
