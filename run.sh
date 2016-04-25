4cdf5c630614d536696a04eca333e9e465ac8080
Modra <754675419@qq.com>
2016-04-25 10:32:37 +0800

    add argv

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #!/mongodb-linux-x86_64-ubuntu1404-3.2.5/bin
 
-python musicSpider.py
+python musicSpider.py $1
 ../../mongodb-linux-x86_64-ubuntu1404-3.2.5/bin/mongoexport -d netease_music_list -c music_list_info -o /163musicSpider/music_list_info.csv
 python mail.py
